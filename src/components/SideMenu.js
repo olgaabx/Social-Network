03ef42e10a036ae75e59927b7b2f30d047676668
Olga Acosta Bravo <olgaacostab@gmail.com>
2023-02-09 23:56:58 -0500

    adding sidemenu component

---
+++ src/components/SideMenu.js
@@ -0,0 +1,20 @@
+function SideMenu() {
+  return (
+    <di class="lateral-menu-container">
+      <div class="logo-container-home">
+        <img src="https://d9hhrg4mnvzow.cloudfront.net/admissions.prepscholar.com/7ade0971-artificial-intelligence_107g07g07a07a000000028.png" alt="logo" class="logo-home" />
+        <p class="logo-home-title">TecHelp</p>
+      </div>
+
+      <div class="profile-section" id="profile-box"></div>
+          
+      <div class="logout-profile">
+        <button id="logOutS">Cerrar sesión</button>
+        <img src="https://cdn-icons-png.flaticon.com/512/1828/1828427.png" alt="" />
+      </div>
+          
+    </di>
+  )
+}
+
+export { SideMenu };
